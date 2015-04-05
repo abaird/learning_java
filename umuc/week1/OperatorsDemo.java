f4da28cb2a998ee3b060e990778bedbc0b80d955
Alan Baird <alan.baird@livingsocial.com>
2015-04-05 10:02:04 -0500

    add new files

---
+++ umuc/week1/OperatorsDemo.java
@@ -0,0 +1,14 @@
+public static void main(String[] args){
+  System.out.println("Math Operators Demo")
+
+  double restult = 0.0;
+  int intresults = 0;
+
+  // Addition (+)
+  results = 4.1 + 8.1;
+  System.out.println("4.1 + 8.1 = " + results);
+  results = 4.1 + 10.5;
+  System.out.println("4.1 + 10.5 = " + results);
+
+
+}
--- umuc/week1/VariableDemo.java
+++ umuc/week1/VariableDemo.java
@@ -4,11 +4,27 @@ public class VariableDemo
 		byte myByte = 20;
 		short myShort = 2000;
 		int myInt = 20000;
-		long myLong = 20000000L;
+    int myHexInt = 0x00dd;
+    int myBinaryInt = 0b1101;
+		long myLong = 2_000_000_000L;
 		float myFloat = 32.234f;
 		double myDouble = 32.932;
 		boolean myBoolean = true;
+    boolean myFalseBoolean = false;
 		char myChar = 'C';
+    final double SPEEDOFSOUND = 343.6; 
+
+    System.out.println("mybyte: " + myByte);
+    System.out.println("myShort: " + myShort);
+    System.out.println("myInt: " + myInt);
+    System.out.println("myHexInt: " + myHexInt);
+    System.out.println("myBinaryInt: " + myBinaryInt);
+    System.out.println("myLong: " + myLong);
+    System.out.println("myFloat: " + myFloat);
+    System.out.println("myDouble: " + myDouble);
+    System.out.println("myBoolean: " + myBoolean);
+    System.out.println("myChar: " + myChar);
+    System.out.println("SPEEDOFSOUND: " + SPEEDOFSOUND);
 
 	}
 }
